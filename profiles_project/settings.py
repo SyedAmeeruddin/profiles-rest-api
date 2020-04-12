b85bc7f8e03f3e78120f52a77d7f3a7fbe120361
Ameeruddin Syed <ameeruddin.syeds@gmail.com>
2020-04-12 19:01:44 +0530

    Rectified name of project

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'proiles_project.urls'
+ROOT_URLCONF = 'profiles_project.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'proiles_project.wsgi.application'
+WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 
 # Database
